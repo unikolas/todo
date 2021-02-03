3aa91c8f3af1897eca6ea44d1f3ced5216a5db1a
Nik Berezovsky <unikolas@MacBook-Pro.local>
2021-02-03 23:01:08 +0000

    An attempt to fix heroku

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // Routes
 
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('client/index'))
 
 // Create a todo
