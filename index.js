09b6b4cc23ff802c938496c18d5d2a3cb3ae9ba5
Nik Berezovsky <unikolas@MacBook-Pro.local>
2021-02-04 03:22:27 +0000

    last fixes

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(express.json())
 
 const publicPath = path.join(__dirname, 'client/build')
 app.use(express.static(publicPath))
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(publicPath, 'index.html'))
-// })
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+})
 
 // Create a todo
 
