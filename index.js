cfa0f68ebfd6abc788493417fe94cdfe13b9cb91
Nik Berezovsky <unikolas@MacBook-Pro.local>
2021-02-04 21:29:22 +0000

    fixes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 if (process.env.NODE_ENV === 'production') {
     const publicPath = path.join(__dirname, 'client/build')
-    app.use(express.static(publicPath))
+    app.use(express.static('client/build'))
     app.get('/', (req, res) =>
         res.sendFile(path.join(publicPath, 'index.html'))
     )
