3a36ee89cbec6796e54640b319df6858bb86effb
Nik Berezovsky <unikolas@MacBook-Pro.local>
2021-02-04 21:45:37 +0000

    fixes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, buildPath)))
-    app.get('/*', (req, res) =>
+    app.get('/', (req, res) =>
         res.sendFile(path.join(__dirname, buildPath, 'index.html'))
     )
     console.log('Prod, ', PORT, ', ', path.join(__dirname))
